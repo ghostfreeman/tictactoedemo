33a2063cbaddb3ef0b7776e18418d675dbcdb173
Cameron Kilgore <ghostfreeman@gmail.com>
2015-12-07 22:53:37 -0500

    Added the last field & method to Player class

--- js/class/player.js
+++ js/class/player.js
@@ -7,9 +7,10 @@
  * Player class
  * @constructor
  */
-function Player(name, symbol) {
+function Player(name, symbol, isHuman) {
   this.name = typeof name !== 'undefined' ? name : "Chopped Liver";
   this.symbol = symbol;
+  this.isHumanPlayer = typeof isHuman !== 'undefined' ? isHuman : true;
 }
 
 /**
@@ -17,4 +18,11 @@ function Player(name, symbol) {
  */
 Player.prototype.changeSymbol = function(symbol) {
   this.symbol = symbol;
+}
+
+/**
+ * Change the player name
+ */
+Player.prototype.changeName = function(name) {
+  this.name = name;
 }
