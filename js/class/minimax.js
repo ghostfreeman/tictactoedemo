f333c34c125fb17b2d021e1c9ee59d89a5eeddf3
Cameron Kilgore <ghostfreeman@gmail.com>
2015-12-09 22:15:54 -0500

    Breakpoint - began work on the important part of the system, the AI

--- js/class/minimax.js
+++ js/class/minimax.js
@@ -7,6 +7,31 @@
  * Minimax Class
  * @constructor
  */
-function Minimax() {
+function Minimax(max_depth) {
+  this.MAXDEPTH = typeof max_depth !== 'undefined' ? max_depth : 7;
+}
 
+/**
+ * The Minimax method
+ */
+Minimax.prototype.minimaxCalc = function() {
+
+}
+
+/**
+ * Recursive tree builder
+ */
+Minimax.prototype.BuildGameTree = function() {
+
+}
+
+/**
+ * Alter the Max Depth attribute for the Minimax tree. By default, it initalizes
+ * at seven.
+ *
+ * I mean you could just change the value at the instance because JavaScript doesn't
+ * do protected or private fields but what would be the fun in that?
+ */
+Minimax.prototype.alterMaxDepth = function(max_depth) {
+  this.MAXDEPTH = max_depth;
 }
